039d3731c786bdc22ceac2c8a8ddd2cb6280c5c8
riguang <zscboy@gmail.com>
2025-02-19 10:14:25 +0800

    install script

--- bandwidth_scheduler.py
+++ bandwidth_scheduler.py
@@ -33,12 +33,20 @@ def get_current_rate(interface):
 # 限制带宽
 def set_bandwidth_limit(interface, rate):
      print(f"set_bandwidth_limit {interface}:{rate}")
-     subprocess.run(f"tc qdisc add dev {interface} root tbf rate {rate} burst 8192kbit latency 300ms", shell=True)
+     try:
+        subprocess.run(f"tc qdisc add dev {interface} root tbf rate {rate} burst 8192kbit latency 300ms", shell=True)
+     except subprocess.CalledProcessError as e:
+        print(f"set_bandwidth_limit error: {e}")
+
 
 # 解除限速
 def del_bandwidth_limit(interface):
     print(f"del_bandwidth_limit {interface}")
-    subprocess.run(f"tc qdisc del dev {interface} root 2>/dev/null", shell=True)  # 删除旧规则
+    try:
+        subprocess.run(f"tc qdisc del dev {interface} root 2>/dev/null", shell=True)  # 删除旧规则
+    except subprocess.CalledProcessError as e:
+        print(f"del_bandwidth_limit error: {e}")
+
 
 def print_unlimit_times(json_config):
     for timer in json_config["no_limit_times"]:
@@ -61,21 +69,40 @@ def handle_bandwidth(json_config, interface):
     else:
         print(f"Bandwith no change")
 
-
+## get network interface
+def get_first_macvtap_interface():
+    try:
+        # 执行 `ip link` 命令，获取所有网络接口信息
+        result = subprocess.check_output("ip link", shell=True, text=True)
+        # 筛选包含 "macvtap" 的行并返回第一个接口名
+        macvtap_interfaces = [line.split(":")[1].strip().split('@')[0] for line in result.splitlines() if "macvtap" in line]
+        return macvtap_interfaces[0] if macvtap_interfaces else None
+    except subprocess.CalledProcessError as e:
+        print(f"Error occurred: {e}")
+        return None
 
 
 def main():
     parser = argparse.ArgumentParser(description="Manage bandwidth based on time intervals.")
     parser.add_argument("--config_file", help="Path to the JSON configuration file.")
     parser.add_argument("--function", choices=["handle_bandwidth", "unlimit_rate"], default="handle_bandwidth", help="Select the function to execute")
-    parser.add_argument("--interface", help="Network interface to manage bandwidth.", required=True)
+    parser.add_argument("--interface", help="Network interface to limit bandwidth.")
 
 
     args = parser.parse_args()
+    
+    interface = args.interface
+    if interface == None:
+        interface = get_first_macvtap_interface()
+
+    if interface == None:
+        print(f"error: the following arguments are required: --interface")
+        return
+
 
     if args.function == "unlimit_rate":
         # Remove bandwidth limit
-        del_bandwidth_limit(args.interface) 
+        del_bandwidth_limit(interface) 
         print(f"Bandwidth limit removed.")
         # Exit after removing the limit
         return 
@@ -84,11 +111,11 @@ def main():
         print(f"error: the following arguments are required: --config_file")
         return
 
-    with open(args.config_file, "r") as f:
-        json_config = json.load(f)
-
     while True:
-        handle_bandwidth(json_config, args.interface)
+        with open(args.config_file, "r") as f:
+            json_config = json.load(f)
+
+        handle_bandwidth(json_config, interface)
         # 每隔 60 秒执行一次
         time.sleep(60)
 
---
+++ install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+
+install_bandwidth_limit() {
+    ### 检查frpc是否存在
+    if systemctl list-units --type=service --all | awk '{print $1}' | grep -x "bandwidth-limit.service"; then
+        echo "bandwidth-limit.service exists"
+    else
+        echo "bandwidth-limit.service does not exist, install..."
+
+        # 下载 zip 文件
+        wget -O /tmp/bandwidth-limit.zip https://codeload.github.com/zscboy/bandwidth-limit/zip/refs/tags/0.0.1
+
+        # 进入 /usr/local 目录
+        cd /usr/local
+
+        # 解压 zip 文件，并去掉版本号
+        unzip /tmp/bandwidth-limit.zip
+        mv bandwidth-limit-0.0.1 bandwidth-limit
+
+        mkdir /etc/bandwidth-limit
+        ## 保留原配置
+        cp -n bandwidth-limit/bandwidth_scheduler.json /etc/bandwidth-limit
+
+        ###TODO　创建web软连接
+        # 清理临时文件
+        rm /tmp/bandwidth-limit.zip
+
+        ###　get interface
+        interface=$(ip -o link show up | awk -F': ' '{print $2}' | while read iface; do [ "$(cat /sys/class/net/$iface/speed 2>/dev/null)" = "10000" ] && echo $iface; done)
+        ## set default interface eth0
+        interface=${interface:-eth0}
+
+        SERVICE_FILE="/etc/systemd/system/bandwidth-limit.service"
+        cat >$SERVICE_FILE <<EOF
+[Unit]
+Description=My Bandwidth Control Service
+After=network.target
+
+[Service]
+ExecStart=/usr/bin/python3 -u /usr/local/bandwidth-limit/bandwidth_scheduler.py --config_file /etc/bandwidth-limit/bandwidth_scheduler.json --interface $interface
+ExecStopPost=/usr/bin/python3 -u /usr/local/bandwidth-limit/bandwidth_scheduler.py --function unlimit_rate --interface $interface
+StandardOutput=journal
+StandardError=journal
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+
+EOF
+
+
+        systemctl enable bandwidth-limit
+        systemctl start bandwidth-limit
+
+    fi
+}
+
+update() {
+    delete
+    install_bandwidth_limit
+}
+
+delete() {
+    systemctl stop bandwidth-limit
+    systemctl disable bandwidth-limit
+
+    rm /etc/systemd/system/bandwidth-limit.service
+    rm -rf /usr/local/bandwidth-limit
+}
+
+
+function main() {
+    case "$1" in
+    install)
+        install_bandwidth_limit
+        ;;
+    update)
+        update
+        ;;
+    delete)
+        delete
+        ;;    
+    *)
+    echo "Usage: $0 {install|update|delete}"
+    exit 1
+    ;;
+esac
+}
+
+main "$1"
